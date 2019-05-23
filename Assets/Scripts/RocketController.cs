c8e4ded35ac66c25d73300f3ec8ffeb2d280550b
Jonatan Karlsson <JK98@Live.se>
2019-05-23 15:46:37 +0200

    Initial commit

---
+++ Assets/Scripts/RocketController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour
+{
+    public Transform direction = null;
+
+    public GameObject ship;
+    public Rigidbody rocketRB;
+    public float RocketForce;
+
+    void Start()
+    {
+        rocketRB = GetComponent<Rigidbody>();
+    }
+
+   
+    void FixedUpdate()
+    {
+        if (this.direction != null && Input.GetKeyDown(KeyCode.Space))
+        {
+            rocketRB.AddForce(direction.right*RocketForce);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+
+        }
+    }
+}
