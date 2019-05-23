1821374b91b76b67bf0fab63894a47e12f43c4da
BlitzBurn <JK98@Live.se>
2019-05-23 23:47:55 +0200

    UpdatedPlayerController
    There is still shit to fix

--- Assets/Scripts/RocketController.cs
+++ Assets/Scripts/RocketController.cs
@@ -6,11 +6,11 @@ public class RocketController : MonoBehaviour
 {
     public Transform direction = null;
 
-    public GameObject ship;
+    
     public Rigidbody rocketRB;
 
     public float rocketForce;
-    public float rotationSpeed;
+   
 
     void Start()
     {
@@ -20,19 +20,11 @@ public class RocketController : MonoBehaviour
    
     void FixedUpdate()
     {
-        if (this.direction != null && Input.GetKeyDown(KeyCode.Space))
+        if (this.direction != null && Input.GetKey(KeyCode.Space))
         {
             rocketRB.AddForce(direction.right*rocketForce);
         }
 
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            ship.transform.Rotate(Vector3.left*rotationSpeed);
-        }
-
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            ship.transform.Rotate(Vector3.right * rotationSpeed);
-        }
     }
+
 }
