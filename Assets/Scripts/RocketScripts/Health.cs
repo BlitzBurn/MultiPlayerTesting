456da684b09fdd29835ae29a3438d0b6e4110d60
Jonatan Karlsson <JK98@Live.se>
2019-05-28 16:57:43 +0200

    Uppdatering

---
+++ Assets/Scripts/RocketScripts/Health.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    public static float HealtPoints;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
