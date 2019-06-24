f002f6316e03c74badf8c017f7bff5f3af441ee6
BlitzBurn <JK98@Live.se>
2019-06-24 14:27:46 +0200

    Started working on missile
    6-24

---
+++ Assets/Scripts/MissileScripts/MoveMissile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMissile : MonoBehaviour
+{
+    public Transform direction = null;
+    public Rigidbody missileRB;
+    public float missileForce;
+
+    void Start()
+    {
+        missileRB = GetComponent<Rigidbody>();
+    }
+
+    
+
+    void Update()
+    {
+        if (this.direction != null )
+        {
+            missileRB.AddForce(direction.up * missileForce);
+        }
+    }
+}
---
+++ Assets/Scripts/RocketScripts/FireMissile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireMissile : MonoBehaviour
+{
+    public GameObject Missile;
+    public GameObject Rocket;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+           
+            Vector3 SpawnPoint =  new Vector3(0, 0, 0);
+
+            Instantiate(Missile, SpawnPoint, Rocket.transform.rotation);
+        }
+
+    }
+}
