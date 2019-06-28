8ccb032f846af9a6d6fc7de92279e413bcbe6382
Jonatan Karlsson <JK98@Live.se>
2019-06-28 20:36:29 +0200

    more shet

--- Assets/Scripts/RocketScripts/FireMissile.cs
+++ Assets/Scripts/RocketScripts/FireMissile.cs
@@ -6,9 +6,12 @@ public class FireMissile : MonoBehaviour
 {
     public GameObject Missile;
     public GameObject Rocket;
+    public Transform RocketDirection;
 
     public float MissileCooldown;
     private float time;
+
+    float MissileSpawnRotation;
 
     // Start is called before the first frame update
     void Start()
@@ -20,14 +23,30 @@ public class FireMissile : MonoBehaviour
     {
         time += Time.deltaTime ;
         //c.transform.rotation.eulerAngles.x
-        Debug.Log(Rocket.transform.rotation.eulerAngles.y);
+      //  Debug.Log(Rocket.transform.rotation.eulerAngles.y);
         if (Input.GetKeyDown(KeyCode.Mouse1) && time>=MissileCooldown)
         {
            
             Vector3 SpawnPoint =  new Vector3(0, 0, 0);
 
-            Instantiate(Missile, SpawnPoint, Quaternion.Euler(90, Rocket.transform.rotation.eulerAngles.y, 0));
+            // AngleAboutY(Rocket) = MissileSpawnRotation; Quaternion.Euler(90, Rocket.transform.rotation.eulerAngles.y, 0)
+
+            /*
+            Vector3 objFwd = Rocket.forward;
+            float angle = Vector3.Angle(objFwd, Vector3.forward);
+            float sign = Mathf.Sign(Vector3.Cross(objFwd, Vector3.forward).y);
+            MissileSpawnRotation = angle * sign;*/
+
+            Instantiate(Missile, SpawnPoint, RocketDirection.rotation);
             time = 0;
+        }
+
+        float AngleAboutY(Transform Rocket)
+        {
+            Vector3 objFwd = Rocket.forward;
+            float angle = Vector3.Angle(objFwd, Vector3.forward);
+            float sign = Mathf.Sign(Vector3.Cross(objFwd, Vector3.forward).y);
+            return angle * sign;
         }
 
     }
